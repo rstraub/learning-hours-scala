6a24f8c3ff961d02b0716c49f20b40a811d72864
Roy <rstraub@xebia.com>
2023-09-11 13:12:19 +0200

    For comprehensions

---
+++ src/test/scala/nl/codecraftr/scala/learninghours/partone/ForComprehensionSpec.scala
@@ -0,0 +1,38 @@
+package nl.codecraftr.scala.learninghours.partone
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ForComprehensionSpec extends AnyFlatSpec with Matchers {
+  private case class Owner(name: String, pets: List[Pet])
+  private case class Pet(
+      name: String,
+      age: Int,
+      favoriteDish: Option[String] = None
+  )
+
+  private val joe =
+    Owner("Joe", List(Pet("Garfield", 42, Some("Lasagne")), Pet("Nermal", 9)))
+  private val jane =
+    Owner(
+      "Jane",
+      List(
+        Pet("Tom", 7, Some("Pizza")),
+        Pet("Sylvester", 13),
+        Pet("Tweety", 2, Some("Seeds"))
+      )
+    )
+
+  "for comprehensions" should "be used to chain flatMaps and maps" in {
+    // TODO extract the names of pets using a for comprehension
+    // Tip: you can "desugar" a for comprehension in intellij to see what it looks like under the hood
+
+//    result shouldBe List("Garfield", "Nermal", "Tom", "Sylvester", "Tweety")
+  }
+
+  it should "be used to filter too" in {
+    // TODO extract the names of pets that are older than 10 and have a favorite dish using a for comprehension
+
+//    result shouldBe List(("Garfield" -> "Lasagne"))
+  }
+}
