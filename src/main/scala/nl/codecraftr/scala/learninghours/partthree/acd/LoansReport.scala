8f7c18f83fa5f9d1417d1b928d869ddefe859b22
Roy Straub <rstraub@xebia.com>
2023-11-13 11:34:20 +0100

    Solution for LH part three

--- src/main/scala/nl/codecraftr/scala/learninghours/partthree/acd/LoansReport.scala
+++ src/main/scala/nl/codecraftr/scala/learninghours/partthree/acd/LoansReport.scala
@@ -1,16 +1,58 @@
 package nl.codecraftr.scala.learninghours.partthree.acd
 
+import nl.codecraftr.scala.learninghours.partthree.acd.Term.{
+  LongTerm,
+  ShortTerm
+}
+
+// Data, Domain representation
 case class LoanId(value: String) extends AnyVal
-
-case class LoansReport(loans: Map[LoanId, Loan]) {
-  def lines: List[String] = {
-    val loanCountLine = s"${loans.size} Book loaned out"
-    val loanLines = loans.map { case (id, loan) =>
-      s"Loan '${id.value}' is '${loan.book}' by '${loan.renter}'"
-    }.toList
-
-    "Loans report:" :: loanCountLine :: loanLines
+case class Loan(book: String, renter: String, term: Term)
+sealed trait Term
+object Term {
+  case object ShortTerm extends Term {
+    override def toString = "Short term"
+  }
+  case object LongTerm extends Term {
+    override def toString = "Long term"
   }
 }
 
-case class Loan(book: String, renter: String)
+case class LoansReport(loans: Map[LoanId, Loan] /* Data */ ) {
+  // Calculation
+  def lines: List[String] = {
+    val loanCountLine = s"${loans.size} Books loaned out"
+
+    val shortTermLines = termLines(ShortTerm)
+    val longTermLines = termLines(LongTerm)
+
+    "Loans report:" :: loanCountLine :: shortTermLines ++ longTermLines
+  }
+
+  private def termLines(term: Term) =
+    s"$term loans:" :: loans
+      .filter(_._2.term == term)
+      .map { case (id, loan) =>
+        loanLine(id, loan)
+      }
+      .toList
+
+  private def loanLine(id: LoanId, loan: Loan) =
+    s"Loan '${id.value}' is '${loan.book}' by '${loan.renter}'"
+}
+
+// Action
+object Logger {
+  def log(lines: List[String]): Unit =
+    lines.foreach(println)
+}
+
+// Action
+object Writer {
+  def write(lines: List[String]): Unit = {
+    import java.io._
+    val pw = new PrintWriter(new File("target/report.txt"))
+    lines.foreach(pw.println)
+    pw.close()
+  }
+}
--- src/test/scala/nl/codecraftr/scala/learninghours/partthree/acd/LoansReportSpec.scala
+++ src/test/scala/nl/codecraftr/scala/learninghours/partthree/acd/LoansReportSpec.scala
@@ -1,27 +1,41 @@
 package nl.codecraftr.scala.learninghours.partthree.acd
 
+import nl.codecraftr.scala.learninghours.partthree.acd.Term.{
+  LongTerm,
+  ShortTerm
+}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class LoansReportSpec extends AnyFlatSpec with Matchers {
-  it should "should create lines of the report" in {
-    val report = LoansReport(
-      Map(
-        LoanId("1") -> Loan("The Lord of the Rings", "J.R.R Tolkien"),
-        LoanId("2") -> Loan("Game of Thrones", "George RR Martin")
-      )
+  private val report = LoansReport(
+    Map(
+      LoanId("1") -> Loan("The Lord of the Rings", "J.R.R Tolkien", ShortTerm),
+      LoanId("2") -> Loan("Game of Thrones", "George RR Martin", LongTerm),
+      LoanId("3") -> Loan("The Dark Tower", "Stephen King", LongTerm),
+      LoanId("4") -> Loan("Dune", "Frank Herbert", ShortTerm)
     )
+  )
 
-    val result = report.lines
-
-    result shouldBe
+  it should "should create lines of the report" in {
+    report.lines shouldBe
       List(
         "Loans report:",
-        "2 Book loaned out",
+        "4 Books loaned out",
+        "Short term loans:",
         "Loan '1' is 'The Lord of the Rings' by 'J.R.R Tolkien'",
-        "Loan '2' is 'Game of Thrones' by 'George RR Martin'"
+        "Loan '4' is 'Dune' by 'Frank Herbert'",
+        "Long term loans:",
+        "Loan '2' is 'Game of Thrones' by 'George RR Martin'",
+        "Loan '3' is 'The Dark Tower' by 'Stephen King'"
       )
   }
 
+  it should "log the report" in {
+    Logger.log(report.lines)
+  }
 
+  it should "write report to a file" in {
+    Writer.write(report.lines)
+  }
 }
