9203fa572f7e34ba97e94f149c661dcc4736c6c1
Roy Straub <rstraub@xebia.com>
2023-11-13 11:09:50 +0100

    Simple report

--- src/main/scala/nl/codecraftr/scala/learninghours/partthree/acd/LoansReport.scala
+++ src/main/scala/nl/codecraftr/scala/learninghours/partthree/acd/LoansReport.scala
@@ -2,7 +2,15 @@ package nl.codecraftr.scala.learninghours.partthree.acd
 
 case class LoanId(value: String) extends AnyVal
 
-case class LoansReport(trackOrigins: Map[LoanId, Loan])
+case class LoansReport(loans: Map[LoanId, Loan]) {
+  def lines: List[String] = {
+    val loanCountLine = s"${loans.size} Book loaned out"
+    val loanLines = loans.map { case (id, loan) =>
+      s"Loan '${id.value}' is '${loan.book}' by '${loan.renter}'"
+    }.toList
+
+    "Loans report:" :: loanCountLine :: loanLines
+  }
+}
 
 case class Loan(book: String, renter: String)
-
--- src/test/scala/nl/codecraftr/scala/learninghours/partthree/acd/LoansReportSpec.scala
+++ src/test/scala/nl/codecraftr/scala/learninghours/partthree/acd/LoansReportSpec.scala
@@ -4,7 +4,24 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class LoansReportSpec extends AnyFlatSpec with Matchers {
-    it should "" in {
+  it should "should create lines of the report" in {
+    val report = LoansReport(
+      Map(
+        LoanId("1") -> Loan("The Lord of the Rings", "J.R.R Tolkien"),
+        LoanId("2") -> Loan("Game of Thrones", "George RR Martin")
+      )
+    )
 
-    }
+    val result = report.lines
+
+    result shouldBe
+      List(
+        "Loans report:",
+        "2 Book loaned out",
+        "Loan '1' is 'The Lord of the Rings' by 'J.R.R Tolkien'",
+        "Loan '2' is 'Game of Thrones' by 'George RR Martin'"
+      )
+  }
+
+
 }
